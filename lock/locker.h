ebb5532c3f60e1bb83fbeeb3ae3749958f25454c
dragonlogin <1241910692@qq.com>
2020-09-09 14:57:04 +0800

    信号量，锁，条件变量的RAII封装

---
+++ lock/locker.h
@@ -0,0 +1,45 @@
+//
+// Created by nl on 2020/9/9.
+//
+#ifndef NLWEBSERVER_LOCKER_H
+#define NLWEBSERVER_LOCKER_H
+#include <pthread.h>
+#include <semaphore.h>
+#include <exception>
+
+// 对信号量的 RAII封装
+class Sem {
+public:
+    Sem();
+    explicit Sem(int num);
+    ~Sem();
+    bool wait();
+    bool post();
+private:
+    sem_t m_sem;
+};
+
+class Locker {
+public:
+    Locker();
+    ~Locker();
+    bool lock();
+    bool unlock();
+    pthread_mutex_t* get();
+private:
+    pthread_mutex_t m_mutex;
+};
+
+
+class Cond {
+public:
+    Cond();
+    ~Cond();
+    bool wait(pthread_mutex_t *m_mutex);
+    bool timewait(pthread_mutex_t *m_mutex, struct timespec t);
+    bool signal();
+    bool broadcast();
+private:
+    pthread_cond_t m_cond;
+};
+#endif //NLWEBSERVER_LOCKER_H
